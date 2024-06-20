5eadfa679ceb79d183d63b5a3922b492a5c5bdb9
Toby Yip <63656845+OurFunMedia@users.noreply.github.com>
2024-06-20 11:16:27 +0800

    Add files via upload

---
+++ demo/us_stock/app.js
@@ -0,0 +1,21 @@
+const apiKey = 'E0Z58TSTYDH2TDIP';
+const symbol = 'NVDA';
+const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`;
+
+async function fetchStockPrice() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const price = data['Global Quote']['05. price'];
+    document.getElementById('price').textContent = `$${parseFloat(price).toFixed(2)}`;
+  } catch (error) {
+    console.error('Error fetching stock price:', error);
+  }
+}
+
+function updateStockPrice() {
+  fetchStockPrice();
+  setInterval(fetchStockPrice, 60000); // 每分鐘更新一次
+}
+
+updateStockPrice();
